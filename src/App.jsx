7de21e54bf2f719000fbf9e4aedcfaf2f8914ecc
Guruju Ganesh Sri Karthikeya <2100032290cseh@gmail.com>
2024-06-26 22:19:41 +0530

    Create the Currency Converter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ConverterForm from "./components/ConverterForm";
+
+const App = () => {
+  return (
+    <div className="currency-conveter">
+      <h2 className="converter-title">Currency Conveter</h2>
+      <ConverterForm />
+    </div>
+  );
+};
+
+export default App;
